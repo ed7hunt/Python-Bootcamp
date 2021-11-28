6888402803e852476dafdb86913de268e09b4bfa
Edward Hunt <57264981+ed7hunt@users.noreply.github.com>
2021-11-28 18:05:33 -0500

    Update leap_year_check.py

--- leap_year_check.py
+++ leap_year_check.py
@@ -3,10 +3,10 @@ year = int(input("Which year do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-if (year % 4) == 0:
-    if (year % 400) == 0:
+if year % 4 == 0:
+    if year % 400 == 0:
         print("Leap year.")
-    elif (year % 100) == 0:
+    elif year % 100 == 0:
         print("Not leap year.")
     else:
         print("Leap year.")
