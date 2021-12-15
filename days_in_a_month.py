8e7852f7f6f28449f03fc756824cabd1ac44134d
Edward Hunt <57264981+ed7hunt@users.noreply.github.com>
2021-12-15 10:53:54 -0500

    Add files via upload

---
+++ days_in_a_month.py
@@ -0,0 +1,27 @@
+# This script finds the number of days in a month given any year including leap years
+
+
+def is_leap(the_year):
+    if year % 4 == 0:
+        if year % 400 == 0:
+            return True
+        elif year % 100 == 0:
+            return False
+        else:
+            return True
+    else:
+        return False
+
+
+def days_in_month(the_year, the_month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(the_year):
+        month_days[1] = 29
+    return month_days[the_month - 1]
+
+
+# 🚨 Do NOT change any of the code below
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
