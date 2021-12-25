a623353bb99049a29109913dbffbfc754d86cda5
Edward Hunt <57264981+ed7hunt@users.noreply.github.com>
2021-12-25 09:59:45 -0500

    Update password_generator.py
    This software is free to use!

--- password_generator.py
+++ password_generator.py
@@ -1,33 +1,61 @@
 import random
 
-# Password Generator Project
-lc_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-uc_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+'''
+Example of usage:
+    
+    from password_generator import generate        # place this line at the top of your Python script to import
+    your_password = generate()                     # place this line inside your script to call to store 'your_password'                                         
+    
+PASSWORD GENERATOR LICENSE
+    Copyright (c) 2021, Edward Hunt <ed7hunt@gmail.com>
+    PERMISSION TO USE, COPY, MODIFY, AND/OR DISTRIBUTE THIS SOFTWARE FOR ANY
+    PURPOSE WITH OR WITHOUT FEE IS HEREBY GRANTED, PROVIDED THAT THE ABOVE
+    COPYRIGHT NOTICE AND THIS PERMISSION NOTICE APPEAR IN ALL COPIES.
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+'''
 
-print("Welcome to the PyPassword Generator!")
-nlc_letters = int(input("How many lowercase letters would you like in your password?\n"))
-nuc_letters = int(input("How many uppercase letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+# globals
+password_length = 12         # total password string length
+upper_max_total = 3          # maximum total uppercase characters
+upper_min_total = 1          # minimum total uppercase characters
+special_char_max_total = 2   # maximum total special characters
+special_char_min_total = 1   # minimum total special characters
+number_max_total = 2         # maximum total numbers
+number_min_total = 1         # minimum total numbers
 
-password = []
-for char in range(1, nlc_letters + 1):
-    password += random.choice(lc_letters)
-for char in range(1, nuc_letters + 1):
-    password += random.choice(uc_letters)
-for char in range(1, nr_symbols + 1):
-    password += random.choice(symbols)
-for char in range(1, nr_numbers + 1):
-    password += random.choice(numbers)
 
-print(password)
-random.shuffle(password)
-print(password)
+def generate():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    special_characters = "@#$%^&?"
+    numbers = "0123456789"
+    uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    password = ""
+    for a in range(random.randint(special_char_min_total, special_char_max_total)):
+        special_char = special_characters[random.randint(0, len(special_characters)-1)]
+        # print(f"special char {(a + 1)}: {special_char}")
+        password += special_char
+    for b in range(random.randint(number_min_total, number_max_total)):
+        number = numbers[random.randint(0, len(numbers)-1)]
+        # print(f"      number {(b + 1)}: {number}")
+        password += number
+    for c in range(random.randint(upper_min_total, upper_max_total)):
+        upper = uppers[random.randint(0, len(uppers)-1)]
+        # print(f"       upper {(c + 1)}: {upper}")
+        password += upper
+    for d in range(len(password), password_length):
+        next_letter_index = random.randrange(len(alphabet))
+        password += alphabet[next_letter_index]
+    password_list = list(password)
+    random.shuffle(password_list)
+    password_result = (''.join(password_list))
+    # print(f"      Password: {password_result}")
+    return password_result
 
-output = ""
-for char in password:
-    output += char
 
-print(output)
+generate()
